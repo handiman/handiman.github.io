8234492abd112c59846c6452de2623cd1d222ec9
Henrik <henrik.becker@hotmail.com>
2017-07-29 11:53:07 +0200

    Resume.json

---
+++ _plugins/jsonresume.rb
@@ -0,0 +1,42 @@
+module Jekyll
+	require 'json'
+	
+	class JSONResumeGenerator < Generator
+		def generate(site)
+			projects = []
+			for projectorder in site.data["projectorder"]
+				project = site.data["projects"][projectorder["name"]]
+				projects << { 
+					"name" => project["title"],
+					"position" => project["position"],
+					"website" => project["url"],
+					"startDate" => project["start_date"],
+					"endDate" => project["end_date"],
+					"summary" => project["description"],
+					"higlights" => project["achievements"]
+				}
+			end
+
+			for profile in site.data["profile"]
+				json = JSON.generate({
+					"basics" => {
+						"name" => profile["name"],
+						"title" => profile["title"],
+						"website" => profile["url"],
+						"location" => {
+							"region" => profile["work_location"]
+						}
+					},
+					"work" => projects
+				})
+
+				#p profile
+				#p json
+
+				File.write("resume.json", json)
+				site.static_files << Jekyll::StaticFile.new(site, site.source, nil, "resume.json")
+			end
+		end
+	end # JSONResumeGenerator
+
+end # Jekyll
