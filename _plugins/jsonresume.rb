4b980660e6c9302283a8033c1557b5cf852af562
Henrik <henrik.becker@hotmail.com>
2017-07-31 13:38:45 +0200

    Meh

--- _plugins/jsonresume.rb
+++ _plugins/jsonresume.rb
@@ -2,47 +2,86 @@ module Jekyll
 	require 'json'
 	
 	class JSONResumeGenerator < Generator
-		def generate(site)
+		def geteducation(profile)
+			education = []
+			profile["education"].each do |key|
+				education << { 
+					"institution" => key["school"],
+					"area" => key["description"],
+					"startDate" => key["start_year"].to_s + "-01-01",
+					"endDate" => key["end_year"].to_s + "-01-01"
+				}
+			end
+			return education
+		end
+
+		def getskills(profile)
+			skills = []
+			profile["skills"].each do |key, values|
+				skills << { 
+					"name" => key.to_s.capitalize,
+					"keywords" => values
+				}
+			end
+			return skills
+		end
+
+		def getlanguages(profile)
+			languages = []
+			profile["languages"].each do |key|
+				languages << { 
+					"name" => key["name"],
+					"keywords" => key["proficiency"]
+				}
+			end
+			return languages
+		end
+
+		def getinterests(profile)
+			interests = []
+			profile["interests"].each do |key|
+				interests << { 
+					"name" => key
+				}
+			end
+			return interests
+		end
+
+		def getprojects(site)
 			projects = []
 			for projectorder in site.data["projectorder"]
 				project = site.data["projects"][projectorder["name"]]
 				projects << { 
-					"name" => project["title"],
-					"position" => project["position"],
-					"website" => project["url"],
-					"startDate" => project["start_date"],
-					"endDate" => project["end_date"],
-					"summary" => project["description"],
+					"name" => project["title"].to_s,
+					"position" => project["position"].to_s,
+					"website" => project["url"].to_s,
+					"startDate" => project["start_date"].to_s,
+					"endDate" => project["end_date"].to_s,
+					"summary" => project["description"].to_s,
 					"higlights" => project["achievements"]
 				}
 			end
+			return projects
+		end
 
+		def generate(site)
 			for profile in site.data["profile"]
-				skills = []
-				profile["skills"].each do |key, values|
-				  skills << { 
-						"name" => key.to_s.capitalize ,
-						"keywords" => values
-					}
-				end
-				languages = []
-				interests = []
-
 				json = JSON.generate({
 					"basics" => {
 						"name" => profile["name"],
-						"title" => profile["title"],
+						"label" => profile["title"],
 						"website" => profile["url"],
+						"summary" =>  profile["summary"].join("\n"),
 						"location" => {
 							"region" => profile["work_location"]
 						}
 					},
-					"skills" => skills,
-					"work" => projects
+					"skills" => getskills(profile),
+					"languages" => getlanguages(profile),
+					"interests" => getinterests(profile),
+					"education" => geteducation(profile),
+					"work" => getprojects(site)
 				})
-
-				#p profile
-				#p json
 
 				File.write(File.join(site.source, "resume.json"), json)
 				site.static_files << Jekyll::StaticFile.new(site, site.source, '/', "resume.json")
