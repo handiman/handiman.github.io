32a31a8ac2ab40079a64e423138b92b6a261b808
Henrik <handiman@users.noreply.github.com>
2018-09-07 11:41:08 +0200

    Please?

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,3 @@
-/*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
 (function($) {
     "use strict";
 
@@ -30,4 +25,23 @@
             });
         }
     }
+	
+	// jQuery to collapse the navbar on scroll
+	function collapseNavbar() {
+		if ($(".navbar").offset().top > 50) {
+			$(".navbar-fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".navbar-fixed-top").removeClass("top-nav-collapse");
+		}
+	}
+
+	
+	$(setupContactForm);
+	$(collapseNavbar);
+	$(window).scroll(collapseNavbar);
+
+	// Closes the Responsive Menu on Menu Item Click
+	$('.navbar-collapse ul li a').click(function () {
+		$(".navbar-collapse").collapse('hide');
+	});
 })(jQuery);
