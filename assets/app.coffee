ebda13f43d19939113ed809e022be50fa84c62d8
Henrik Becker <handiman@users.noreply.github.com>
2018-10-10 16:10:47 +0200

    Not sold yet, but I like coffee

---
+++ assets/app.coffee
@@ -0,0 +1,14 @@
+---
+---
+collapseNavbar = -> 
+	if $(".navbar").offset().top > 50
+		$(".navbar-fixed-top").addClass "top-nav-collapse" 
+	else 
+		$(".navbar-fixed-top").removeClass "top-nav-collapse"
+
+$ -> 		
+	$(setupContactForm);
+	$(collapseNavbar);
+	$(window).scroll(collapseNavbar);
+	$('.navbar-collapse ul li a').click(-> 
+		$(".navbar-collapse").collapse 'hide');
--- assets/script.js
+++ assets/script.js
@@ -26,22 +26,4 @@
         }
     }
 	
-	// jQuery to collapse the navbar on scroll
-	function collapseNavbar() {
-		if ($(".navbar").offset().top > 50) {
-			$(".navbar-fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".navbar-fixed-top").removeClass("top-nav-collapse");
-		}
-	}
-
-	
-	$(setupContactForm);
-	$(collapseNavbar);
-	$(window).scroll(collapseNavbar);
-
-	// Closes the Responsive Menu on Menu Item Click
-	$('.navbar-collapse ul li a').click(function () {
-		$(".navbar-collapse").collapse('hide');
-	});
 })(jQuery);
