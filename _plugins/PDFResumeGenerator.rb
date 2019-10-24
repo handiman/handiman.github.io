f3a9d146a6c7fe2ed1bde91768cec62699f67b1b
Henrik <handiman@users.noreply.github.com>
2019-10-24 17:01:39 +0200

    PDF resume revisited

--- _plugins/PDFResumeGenerator.rb
+++ _plugins/PDFResumeGenerator.rb
@@ -13,73 +13,99 @@ module Jekyll
 			font_name = "Helvetica"
 			
 			Prawn::Font::AFM.hide_m17n_warning = true
-			Prawn::Document.generate("resume.pdf", :margin => 50) do
+			Prawn::Document.generate("resume.pdf", :margin => 70) do
 				default_leading 3
 				font font_name, :size => 10
 				separator = 15
 			
 				# <Cover>
 					bounding_box([0, 150], :width => 250, :height => 100) do
-						#stroke_bounds
-						text "Henrik Becker", :size => h1
+						text profile["name"], :size => h1
 						stroke_horizontal_rule
 						move_down default_leading * 3
-						text "Software Engineer", :size => h2
+						text profile["title"], :size => h2
 						move_down default_leading * 3
-						text "073-422 83 43"
+						text profile["contact_info"]["phone"]
 						text "www.henrikbecker.net"
 					end
-					start_new_page
 				# </Cover>
-				
+
+				start_new_page
+
 				# <Summary>
+					text "Summary", :size => h1
+					profile["summary"].each do | item | 
+						text item
+					end
+				# </Summary>
+	
+				column_width = bounds.width / 2
+				# <Skills>
+					move_down separator
 					page_top = cursor
-					column_width = bounds.width / 3
-					bounding_box([0, page_top], :width => column_width * 2 - separator) do
-						text "Henrik Becker Is", :size => h1
-						profile["summary"].each do | item | 
-							text item
+				
+					bounding_box([0, page_top], :width => column_width - separator) do
+						text "Frameworks", :size => h2
+						profile["skills"]["frameworks"].each do | framework |
+							text framework
 						end
-						
-						move_down separator
+					end
+
+					bounding_box([column_width + separator, page_top], :width => column_width - separator) do
+						text "Languages (programming)", :size => h2
+						profile["skills"]["languages"].each do | language |
+							text language
+						end
+					end
+					
+					move_down separator
+					move_down separator
+					page_top = cursor
+
+					bounding_box([0, page_top], :width => column_width - separator) do
+						text "Methods", :size => h2
+						profile["skills"]["methods"].each do | method |
+							text method
+						end
+					end
+
+					bounding_box([column_width + separator, page_top], :width => column_width - separator) do
+						text "Tools", :size => h2
+						profile["skills"]["tools"].each do | tool |
+							text tool
+						end
+					end
+					
+					move_down separator
+					move_down separator
+					page_top = cursor
+
+					bounding_box([0, page_top], :width => column_width - separator) do
 						text "Certifications", :size => h2
 						profile["certifications"].each do | cert |
 							text cert["name"]
 						end
-						
+
 						move_down separator
+
 						text "Languages", :size => h2
 						profile["languages"].each do | language |
 							text language["name"] + " - " + language["proficiency"]
 						end
-						
-						move_down separator
-						text "Education", :size => h2
-						profile["education"].each do | education |
-							text education["school"]
-						end
 					end
-				# </Summary>
-	
-				# <Skills>
-					bounding_box([column_width * 2, page_top], :width => column_width) do
-						text "Skill Summary", :size => h1
-						profile["skills"].each do | key, values |
-							font font_name, :style => :bold do
-								text key.capitalize, :size => h4
-							end
-							values.each do | value |
-								text value
-							end
-							move_down separator
+
+					bounding_box([column_width + separator, page_top], :width => column_width - separator) do
+						text "Databases", :size => h2
+						profile["skills"]["databases"].each do | database |
+							text database
 						end
 					end
 				# </Skills>
+
 				start_new_page
 				
 				# <Projects>
 					text "Project History", :size => h1
-					stroke_horizontal_rule
 					site.collections["projects"].docs.reverse_each do | project |
 						span(500) do
 							move_down separator
@@ -106,7 +132,6 @@ module Jekyll
 				# <Employment>
 					start_new_page
 					text "Employment History", :size => h1
-					stroke_horizontal_rule
 					move_down separator
 					site.collections["employment"].docs.reverse_each do | employment |
 						start_date = employment["start_date"].strftime("%Y-%m")
@@ -115,11 +140,30 @@ module Jekyll
 							end_date = end_date.strftime("%Y-%m")
 						end
 
-						span(500) do
-							text start_date + " - " + end_date + " " + employment["title"] + " at " + employment["organization"]["name"]
+						page_top = cursor
+						bounding_box([0, page_top], :width => 90) do
+							text start_date + " - " + end_date
+						end
+						bounding_box([100, page_top], :width => bounds.width - 100) do
+							text employment["title"] + " at " + employment["organization"]["name"]
 						end
 					end
 				# </Employment>
+
+				# <Education>
+					move_down separator
+					move_down separator
+					text "Education", :size => h2
+					profile["education"].each do | education |
+						page_top = cursor
+						bounding_box([0, page_top], :width => 90) do
+							text education["start_year"].to_s + " - " + education["end_year"].to_s
+						end
+						bounding_box([100, page_top], :width => bounds.width - 100) do
+							text education["school"]
+						end
+					end
+				# </Education>
 			end
 			
 			site.static_files << Jekyll::StaticFile.new(site, site.source, '/', "resume.pdf")
