1d3ff0aaf9cc59ff0194b1360ec578db8065cec3
Henrik <handiman@users.noreply.github.com>
2020-01-31 21:09:59 +0100

    Installing packages

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        "index": "./src/index.js",
+        "worker": "./src/worker.js"
+    },
+    output: {
+	    path: path.resolve(__dirname, "./"),
+	    filename: "[name].js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+            test: /\.css$/,
+            use: [{
+                loader: "style-loader"
+            }, {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        localIdentName: "[local][hash:base64:5]"
+                    }
+                }
+            }]
+        }
+      ]
+    }
+};  
