b39af1f423faad35740d8587afc7a1ce47b62c1e
Henrik <handiman@users.noreply.github.com>
2020-02-03 20:01:37 +0100

    Installed file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,27 @@ module.exports = {
               }, 
               { loader: "less-loader" }
           ],
-        }
+        }/*,
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'fonts/'
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(jpe?g|gif|bmp|png)$/,
+          use: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img/'
+          }
+        }*/
       ]
     }
 };  
