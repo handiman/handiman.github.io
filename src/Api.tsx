ac43876f43b8c117378a696274abc92a757cc996
Henrik Becker <handiman@users.noreply.github.com>
2020-04-28 14:31:43 +0200

    Api version

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,3 +1,5 @@
+const ApiRootUri = 'https://www.henrikbecker.se/api/1.0';
+
 const get = async (resource: string) => {
   const response = await fetch(resource);
   return await response.json();
@@ -7,7 +9,7 @@ const getUsps = async () => await get('/static/usps.json');
 const getRecommendations = async () => await get('/static/recommendations.json');
 
 const getIp = async function () {
-  const response = await fetch('https://www.henrikbecker.se/api/ip');
+  const response = await fetch(`${ApiRootUri}/ip`);
   return await response.text();
 }
 
@@ -26,7 +28,7 @@ const sendContactForm = async (form: {
     body.append("message", message);
 
     var request = new XMLHttpRequest();
-    request.open('POST', 'https://www.henrikbecker.se/api/contact-form');
+    request.open('POST', `${ApiRootUri}/contact-form`);
     request.send(body);
   })
 }
