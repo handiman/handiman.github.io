77bf44c72c10c39c18f556e5c03a1239c84a8193
Henrik Becker <handiman@users.noreply.github.com>
2020-05-14 20:18:01 +0200

    :persevere:

--- src/profile/components/ContactForm.tsx
+++ src/profile/components/ContactForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Typography, Dialog, DialogTitle, DialogActions, DialogContent, TextField, Button, makeStyles } from '@material-ui/core';
-import { Section } from '.';
+import { Section, Loading } from '.';
 import { sendContactForm } from '../../Api';
 
 const useStyles = makeStyles(_ => ({
@@ -22,20 +22,35 @@ const ContactFormDialog = (props: {
   const [from, setFrom] = useState('');
   const [message, setMessage] = useState('');
   const [sent, setSent] = useState(false);
-  const submit = (e: any) => {
+  const [sending, setSending] = useState(false);
+  const submit = async (e: any) => {
     e && e.preventDefault();
-    sendContactForm({ name, from, message }).then(_ => {
-      setSent(true);
-      window.setTimeout(() => {
-        onClose();
-        setSent(false);
-      }, 1500);
-    });
+    setSending(true);
+    await sendContactForm({ name, from, message });
+    setSent(true);
+    setSending(false);
+    window.setTimeout(() => {
+      close();
+      setSent(false);
+    }, 1500);
     return false;
   }
 
+  const reset = () => {
+    setSending(false);
+    setSent(false);
+    setName('');
+    setFrom('');
+    setMessage('');
+  }
+
+  const close = () => {
+    reset();
+    onClose();
+  }
+
   return (
-    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
+    <Dialog open={open} onClose={close} maxWidth="md" fullWidth>
       <DialogTitle>
         Would you like to get in touch?
       </DialogTitle>
@@ -47,6 +62,7 @@ const ContactFormDialog = (props: {
         </Typography>
       </DialogContent>
       <DialogActions>
+        {sending && <Loading />}
         {sent && (<Typography variant="body1" component="span">Message sent</Typography>)}
         <Button onClick={submit} variant="contained" color="primary">Send</Button>
         <Button onClick={onClose} variant="contained" color="secondary">Cancel</Button>
@@ -61,12 +77,14 @@ const ContactForm = () => {
   const [from, setFrom] = useState('');
   const [message, setMessage] = useState('');
   const [sent, setSent] = useState(false);
-  const submit = (e: any) => {
+  const [sending, setSending] = useState(false);
+  const submit = async (e: any) => {
     e && e.preventDefault();
-    sendContactForm({ name, from, message }).then(_ => {
-      setSent(true);
-      window.setTimeout(() => setSent(false), 2000);
-    });
+    setSending(true);
+    await sendContactForm({ name, from, message });
+    setSending(false);
+    setSent(true);
+    window.setTimeout(() => setSent(false), 2000);
     return false;
   }
 
@@ -78,6 +96,7 @@ const ContactForm = () => {
         <TextField className={classes.bottomMargin} fullWidth variant="outlined" size="small" name="from" placeholder="Your e-mail address" value={from} onChange={(e: any) => setFrom(e.target.value)} />
         <TextField className={classes.bottomMargin} fullWidth variant="outlined" size="small" name="message" placeholder="Message" rows={5} value={message} onChange={(e: any) => setMessage(e.target.value)} multiline />
         <Typography component="div" align="right">
+          {sending && <Loading />}
           {sent && (<span>Message sent</span>)}
           <Button type="submit" variant="contained" color="primary">Send</Button>
         </Typography>
