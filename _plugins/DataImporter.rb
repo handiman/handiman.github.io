086be1f4176266326b962478e1d64b0ce45ed997
Henrik Becker <handiman@users.noreply.github.com>
2023-09-21 18:53:46 +0200

    Don't run on win

--- _plugins/DataImporter.rb
+++ _plugins/DataImporter.rb
@@ -7,13 +7,15 @@ module Jekyll
         # Updates _data/person.json with some stuff from elsewhere
         #
         def generate(site)
-            handiman = JSON.parse(RestClient.get('https://api.github.com/users/handiman'))
-            readme = RestClient.get('https://raw.githubusercontent.com/handiman/handiman/master/README.md').lines.first
-            person_path = "#{site.source}/_data/person.json"
-            person = JSON.parse(File.read(person_path))
-            person["description"] = readme
-            person["image"] = handiman["avatar_url"]
-            File.write(person_path, person.to_json)
+            if not (Gem.win_platform?)
+                handiman = JSON.parse(RestClient.get('https://api.github.com/users/handiman'))
+                readme = RestClient.get('https://raw.githubusercontent.com/handiman/handiman/master/README.md').lines.first
+                person_path = "#{site.source}/_data/person.json"
+                person = JSON.parse(File.read(person_path))
+                person["description"] = readme
+                person["image"] = handiman["avatar_url"]
+                File.write(person_path, person.to_json)
+            end                
         end
     end
-end    
+end
