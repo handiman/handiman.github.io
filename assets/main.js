1e517b24338ce96154c125f728407eb004960878
handiman <handiman@users.noreply.github.com>
2026-02-19 20:07:42 +0000

    Update gh-pages to output generated at 8d4391d

--- assets/main.js
+++ assets/main.js
@@ -27,4 +27,33 @@
       card.classList.toggle('flipped');
     });
   });
+
+  document.querySelectorAll('.contact-form').forEach(contactForm => {
+    contactForm.addEventListener('submit', async function(e) {
+      e.preventDefault();
+      const form = e.target;
+      const status = form.querySelector('.status');
+      try {
+        status.textContent = "Sending...";
+        
+        const res = await fetch(form.action, {
+          method: form.action,
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            name: form.name.value,
+            email: form.email.value,
+            body: form.body.value
+          })
+        });
+
+        if (!res.ok) throw new Error("Something went wrong :/");
+
+        status.textContent = "Message sent!";
+        form.reset();
+      } catch (err) {
+        status.textContent = "Couldn't send message :(";
+        console.error(err);
+      }
+    });
+  });
 })();
