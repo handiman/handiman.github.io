153682eb7f29ad820c471eaf0aa5b76f96473a97
handiman <handiman@users.noreply.github.com>
2026-08-07 15:45:10 +0200

    :broom:

--- _assignments/assignments.11tydata.js
+++
@@ -1,9 +0,0 @@
-import { getLocale, localizedPermalink } from "../.eleventy.utils.js";
-
-export default function () {
-  return {
-    layout: "experience",
-    tags: ["assignments"],
-    permalink: (data) => localizedPermalink(data, "assignments"),
-  };
-}
